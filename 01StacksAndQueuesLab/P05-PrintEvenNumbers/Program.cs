5be5efd53279f4f05974e83b900227bef769ab1e
Petko Pedev <petko.pedev@gmail.com>
2019-05-19 19:33:52 +0200

    Updated stacks and queues labs.

---
+++ 01StacksAndQueuesLab/P05-PrintEvenNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P05_PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>(input);
+            //TODO
+            
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 01StacksAndQueuesLab/P08-TrafficJam/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P08_TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>();
+            int count = 0;
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    var carsThatCanPass = Math.Min(queue.Count, n);
+                    for (int counter = 0; counter < carsThatCanPass; counter++)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        count++;
+                    }
+                }
+                else
+                    queue.Enqueue(command);
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+        }
+    }
+}
