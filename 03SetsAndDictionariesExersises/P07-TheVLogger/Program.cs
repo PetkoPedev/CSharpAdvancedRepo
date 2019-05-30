441620cb2ad71d6fe6d082b4f57294343eb02ff5
Petko Pedev <petko.pedev@gmail.com>
2019-05-30 21:10:32 +0200

    I have added Ranking task.

---
+++ 03SetsAndDictionariesExersises/P07-TheVLogger/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P07_TheVLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, HashSet<string>>> vloggerCollection = new Dictionary<string, Dictionary<string, HashSet<string>>>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "Statistics")
+            {
+                if (input.Contains("joined"))
+                {
+                    string username = input.Split()[0];
+                    if (!vloggerCollection.ContainsKey(username))
+                    {
+                        vloggerCollection.Add(username, new Dictionary<string, HashSet<string>>());
+                        vloggerCollection[username].Add("followings", new HashSet<string>());
+                        vloggerCollection[username].Add("followers", new HashSet<string>());
+                    }
+                }
+                else if (input.Contains("followed"))
+                {
+                    string[] usernames = input.Split();
+                    string firstVlogger = usernames[0];
+                    string secondVlogger = usernames[1];
+
+                    if (!vloggerCollection.ContainsKey(firstVlogger) || 
+                        !vloggerCollection.ContainsKey(secondVlogger) || 
+                        firstVlogger == secondVlogger)
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+
+                    vloggerCollection[firstVlogger]["following"].Add(secondVlogger);
+
+                }
+            }
+            
+        }
+    }
+}
