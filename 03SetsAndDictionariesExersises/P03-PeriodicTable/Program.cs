43cefe63e22fe3290fbcc4f52de31009306ea79e
Petko Pedev <petko.pedev@gmail.com>
2019-05-30 11:30:13 +0200

    Updating the Sets and Dictionaries exercises with Periodic Table task.

---
+++ 03SetsAndDictionariesExersises/P03-PeriodicTable/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P03_PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            SortedSet<string> chemicals = new SortedSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+                for (int j = 0; j < input.Length; j++)
+                {
+                    chemicals.Add(input[j]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicals));
+        }
+    }
+}
