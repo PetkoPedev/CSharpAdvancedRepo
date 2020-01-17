0711d647fd2ee11b1046c30957fcbc50a3cb76c1
Petko Pedev <petko.pedev@gmail.com>
2020-01-17 10:32:28 +0200

    Adding Print Even Numbers Task

--- 01StacksAndQueuesLab/P05-PrintEvenNumbers/Program.cs
+++ 01StacksAndQueuesLab/P05-PrintEvenNumbers/Program.cs
@@ -9,11 +9,15 @@ namespace P05_PrintEvenNumbers
         static void Main(string[] args)
         {
             var input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> numbers = new Queue<int>(input);
+            Queue<int> result = new Queue<int>();
 
-            Queue<int> queue = new Queue<int>(input);
-            //TODO
-            
-            Console.WriteLine();
+            foreach (var number in numbers.Where(n => n % 2 == 0))
+            {
+                result.Enqueue(number);
+            }
+
+            Console.WriteLine(string.Join(", ", result));
         }
     }
 }
