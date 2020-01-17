13e5434270b45ea331b06da04047412bf6661d67
Petko Pedev <petko.pedev@gmail.com>
2020-01-17 10:51:32 +0200

    Added Supermarket Task

---
+++ 01StacksAndQueuesLab/P06-Supermarket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P06_Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line = string.Empty;
+            Queue<string> people = new Queue<string>();
+            Queue<string> paidPeople = new Queue<string>();
+
+            while ((line = Console.ReadLine()) != "End")
+            {
+                if (line == "Paid")
+                {
+                    foreach (var p in people)
+                    {
+                        paidPeople.Enqueue(p);
+                        Console.WriteLine(p);
+                    }
+                    people.Clear();
+                }
+                else
+                {
+                    people.Enqueue(line);
+                }
+            }
+
+            Console.WriteLine($"{people.Count} people remaining.");
+        }
+    }
+}
